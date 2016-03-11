b37a1ad91e98333f2008b00ddc7aaca88acb0b40
root <root@worker01-001.rmkr.net>
2016-03-11 04:00:47 -0800

    +fix run

--- rmkr-worker/run.sh
+++ rmkr-worker/run.sh
@@ -1,11 +1,16 @@
 
+
 docker run -it --rm --name worker \
--v /root/zmywptest.com_php:/etc/nginx/sites-enabled/zmywptest.com   \
 -v /etc/nginx/conf.d:/etc/nginx/conf.d  \
+-v /etc/nginx/sites-enabled:/etc/nginx/sites-enabled  \
+-v /var/lib/notify:/var/lib/notify \
+-v /var/lib/extrausers:/var/lib/extrausers \
+-v /etc/php5/fpm/pool.d:/etc/php5/fpm/pool.d \
 -v /var/www:/var/www    \
 -v /etc/group:/etc/group    \
 -v /etc/shadow:/etc/shadow  \
 -v /etc/passwd:/etc/passwd \
 -p 80:80 \
+--log-driver=syslog --log-opt syslog-address=tcp://172.31.17.63   --net=host \
     4d44e7d1c2c1 
 
